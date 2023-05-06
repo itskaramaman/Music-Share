60f68e1ded0a5b664010b36c958fde35b12d747a
itskaramaman <singhkaramjeetaman@gmail.com>
2023-05-06 14:42:08 -0700

    room views done

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.endpoints),
+    path('room-list/', views.room_list, name='room-list')
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Room
+from .serializer import RoomSerialzer
 
-# Create your views here.
+
+@api_view(['GET'])
+def endpoints(request):
+    url_list = ['api/rooms']
+    return Response(url_list)
 
 
-def home(request):
-    return HttpResponse("<h1>Music Controller</h1>")
+@api_view(['GET', 'POST'])
+def room_list(request):
+    if request.method == 'GET':
+        rooms = Room.objects.all()
+        serialzer = RoomSerialzer(rooms, many=True)
+        return Response(serialzer.data)
+
+    if request.method == 'POST':
+        code = request.data['code']
+        host = request.data['host']
+        guest_can_pause = request.data['guest_can_pause']
+        votes_to_skip = request.data['votes_to_skip']
+
+        Room.objects.create(
+            code=code,
+            host=host,
+            guest_can_pause=guest_can_pause,
+            votes_to_skip=votes_to_skip
+        )
+
+        return redirect('room-list')
